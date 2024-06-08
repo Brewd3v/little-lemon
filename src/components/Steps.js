f584eaf09f596b00fb47f9456a1b520b9278e8da
James Marx <james@brewdev.co.za>
2024-06-08 13:54:56 +0100

    step indicator

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,17 +1,36 @@
-export default function Steps() {
+export default function Steps({ step, setStep }) {
+  function handleStepChange(selectedStep) {
+    if (step > selectedStep) {
+      setStep(selectedStep);
+    }
+  }
   return (
     <div className="flex items-center justify-center">
-      <div className="flex items-center justify-center w-8 h-8 border-2 rounded-full bg-primary-lightGray text-primary-green text-cardtitle">
+      <button
+        type="button"
+        className={`${step >= 1 && "bg-primary-lightGray !text-primary-green"} flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
+        onClick={() => handleStepChange(1)}
+        disabled={step <= 1}
+      >
         1
-      </div>
-      <div className="h-[2px] bg-primary-lightGray w-6"></div>
-      <div className="flex items-center justify-center w-8 h-8 border-2 rounded-full text-primary-lightGray text-cardtitle">
+      </button>
+      <span className="h-[2px] bg-primary-lightGray w-6"></span>
+      <button
+        type="button"
+        className={`${step >= 2 && "bg-primary-lightGray !text-primary-green"} flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
+        onClick={() => handleStepChange(2)}
+        disabled={step <= 2}
+      >
         2
-      </div>
-      <div className="h-[2px] bg-primary-lightGray w-6"></div>
-      <div className="flex items-center justify-center w-8 h-8 border-2 rounded-full text-primary-lightGray text-cardtitle">
+      </button>
+      <span className="h-[2px] bg-primary-lightGray w-6"></span>
+      <button
+        type="button"
+        className={`${step >= 3 && "bg-primary-lightGray !text-primary-green"} flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
+        disabled={true}
+      >
         3
-      </div>
+      </button>
     </div>
   );
 }

b3d6a2a2a9272d28808d3a78c4175a923aceef19
James Marx <james@brewdev.co.za>
2024-06-10 10:49:40 +0100

    check completed steps

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,3 +1,5 @@
+import StepCheckIcon from "./icons/StepCheck";
+
 export default function Steps({ step, setStep }) {
   function handleStepChange(selectedStep) {
     if (step > selectedStep) {
@@ -8,28 +10,35 @@ export default function Steps({ step, setStep }) {
     <div className="flex items-center justify-center">
       <button
         type="button"
-        className={`${step >= 1 && "bg-primary-lightGray !text-primary-green"} flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
+        className={`${
+          step >= 1 && "bg-primary-lightGray !text-primary-green"
+        } flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
         onClick={() => handleStepChange(1)}
         disabled={step <= 1}
       >
-        1
+        {step > 1 ? <StepCheckIcon /> : "1"}
       </button>
       <span className="h-[2px] bg-primary-lightGray w-6"></span>
       <button
         type="button"
-        className={`${step >= 2 && "bg-primary-lightGray !text-primary-green"} flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
+        className={`${
+          step >= 2 && "bg-primary-lightGray !text-primary-green"
+        } flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
         onClick={() => handleStepChange(2)}
         disabled={step <= 2}
       >
-        2
+        {step > 2 ? <StepCheckIcon /> : "2"}
       </button>
       <span className="h-[2px] bg-primary-lightGray w-6"></span>
       <button
         type="button"
-        className={`${step >= 3 && "bg-primary-lightGray !text-primary-green"} flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
+        className={`${
+          step >= 3 && "bg-primary-lightGray !text-primary-green"
+        } flex items-center justify-center w-8 h-8 border-2 rounded-full text-cardtitle text-primary-lightGray`}
+        onClick={() => handleStepChange(3)}
         disabled={true}
       >
-        3
+        {step > 3 ? <StepCheckIcon /> : "3"}
       </button>
     </div>
   );
---
+++ src/components/icons/StepCheck.js
@@ -0,0 +1,17 @@
+export default function StepCheckIcon() {
+  return (
+    <svg
+      width="33"
+      height="32"
+      viewBox="0 0 33 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect x="1" y="0.5" width="31" height="31" rx="15.5" stroke="#EDEFEE" />
+      <path
+        d="M16.5 0C7.668 0 0.5 7.168 0.5 16C0.5 24.832 7.668 32 16.5 32C25.332 32 32.5 24.832 32.5 16C32.5 7.168 25.332 0 16.5 0ZM13.3 24L5.3 16L7.556 13.744L13.3 19.472L25.444 7.328L27.7 9.6L13.3 24Z"
+        fill="#495e57"
+      />
+    </svg>
+  );
+}
